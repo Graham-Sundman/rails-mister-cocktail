f2bf7d9a3bcde212beeed67510558c48c94d34ff
Graham Sundman <grahamsundman@hotmail.com>
2018-07-30 16:36:29 +0800

    models controllers views

---
+++ app/models/ingredient.rb
@@ -0,0 +1,11 @@
+class Ingredient < ApplicationRecord
+  has_many :doses
+  has_many :cocktails, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+
+end
+
+
+# id cocktail_id ingredient_id description
+#  1.    3.          6          "jjkjljjljk"
