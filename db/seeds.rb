1c5e4501aa7406aac0288e0da76afec0d4727d43
Graham Sundman <grahamsundman@hotmail.com>
2018-07-31 17:12:52 +0800

    update if statement and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require "json"
 require "open-uri"
 
+Ingredient.destroy_all
+
 url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 uri = URI.parse(url)
 ingredients = uri.open.read
@@ -10,3 +12,11 @@ ingredients_data = json_ingredients["drinks"]
 ingredients_data.each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
+
+Cocktail.destroy_all
+
+Cocktail.create(name: "Long Island Ice Tea")
+Cocktail.create(name: "klnasknl")
+Cocktail.create(name: "sjdh")
+Cocktail.create(name: "gin tonic")
+Cocktail.create(name: "bloddy mary")
